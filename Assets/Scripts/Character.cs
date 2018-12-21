8e8e5935e3034f0383c45a78b23fb476943ddb88
Iban D <delgadoiban1a@gmail>
2018-12-21 04:24:16 +0100

    Damage system

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour, IDamageable {
+
+    public float startingHealth = 100f;
+    protected float health;
+    bool dead = false;
+
+    public virtual void Start() {
+        health = startingHealth;
+    }
+
+    public void TakeDamage(float damage, RaycastHit hit) {
+        health -= damage;
+        if(health<=0 && !dead) {
+            Die();
+        }
+    }
+
+    public void Die() {
+        dead = true;
+        GameObject.Destroy(this.gameObject);
+    }
+}
